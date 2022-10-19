4a738fc7d8917ab28ed6b20c88a739965a65ae00
andrescio <andres.cioppi@bosonit.com>
2022-10-19 12:40:16 +0200

    Eliminado import sin usar

--- block5-command-line-runner/src/main/java/org/example/TerceraClase.java
+++ block5-command-line-runner/src/main/java/org/example/TerceraClase.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
