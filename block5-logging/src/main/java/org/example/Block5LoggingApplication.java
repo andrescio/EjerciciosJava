ae8b62f4e789af8d2e13c356c084b7127dcc7dd5
andrescio <andres.cioppi@bosonit.com>
2022-10-21 13:08:53 +0200

    block5-logging terminado

---
+++ block5-logging/src/main/java/org/example/Block5LoggingApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5LoggingApplication.class, args);
+	}
+
+}
---
+++ block5-logging/src/main/java/org/example/ControladorLog.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ControladorLog {
+    Logger logger = LoggerFactory.getLogger(ControladorLog.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("Mensaje a nivel de TRACE");
+        logger.debug("Mensaje a nivel de DEBUG");
+        logger.info("Mensaje a nivel de INFO");
+        logger.warn("Mensaje a nivel de WARNING");
+        logger.error("Mensaje a nivel de ERROR");
+        return "Hola! Mira los logs para ver los resultados";
+    }
+}
